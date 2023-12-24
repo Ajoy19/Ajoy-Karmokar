87319ef47cda26ec328ea044ce576645dcb12cbe
Ajoy19 <104132781+Ajoy19@users.noreply.github.com>
2023-12-24 22:55:57 +0600

    Add, Autotype & copyright

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.autotype', {
+    strings: ["Office Application Expert", "Graphic Designer", "Web Designer", "Digital Marketer", "It Service Provider"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
