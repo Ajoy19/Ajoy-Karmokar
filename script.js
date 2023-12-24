93541e5c2a7523a88823702f3bb70af34bd41de1
Ajoy19 <104132781+Ajoy19@users.noreply.github.com>
2023-12-25 03:47:56 +0600

    final project 25'dec

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.autotype', {
-    strings: ["Office Application Expert", "Graphic Designer", "Web Designer", "Digital Marketer", "It Service Provider"],
+    strings: ["Office Application Expert", "Graphic Designer", "Web Designer", "Digital Marketer", "IT Service Provider"],
     typeSpeed: 50,
     backSpeed: 50,
     loop: true
